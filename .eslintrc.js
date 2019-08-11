6899ea2695696416aa70a74651a1180ea048df7d
Jan David <jandavid@6a64.com>
2019-08-11 18:38:00 +0200

    Enforce a consistent JavaScript style
    For JavaScript, Prettier has been installed as an auto-formatter, and
    ESLint as a linter. Both have been integrated with each other through
    the plugins Prettier provides for ESLint.

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    commonjs: true
+  },
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  rules: {}
+};
