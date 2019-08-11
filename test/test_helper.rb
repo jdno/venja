2f92330d36678a337b0ace5db52a3ce5818b2778
Jan David <jandavid@6a64.com>
2019-08-11 19:44:25 +0200

    Measure test coverage
    As a proxy for code quality, we are measuring the test coverage. The
    goal will be to keep the test coverage above ~90%, but this is not going
    to be enforced.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 ENV["RAILS_ENV"] ||= "test"
+
+require "simplecov"
+SimpleCov.start "rails"
+
 require_relative "../config/environment"
 require "rails/test_help"
 
