98073f7c2fd2a3805380bba0d6d5b9a28938590e
Jan David <jandavid@6a64.com>
2019-08-11 21:43:12 +0200

    Serve static pages with HighVoltage
    HighVoltage is a gem that makes it easy to serve static pages in Rails.
    It has been configured with future features in mind, specifically a
    different root path for authenticated users. A static home page has been
    created, but not customized yet.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file,
+  # see https://guides.rubyonrails.org/routing.html
+
+  get "/pages/*id" => "pages#show", as: :page, format: false
+
+  root to: "pages#show", id: "home"
 end
