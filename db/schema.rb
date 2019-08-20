b5a57d35c06a66d3f16184f30352a08a8cbeedf8
Jan David <jandavid@6a64.com>
2019-08-20 14:39:33 +0200

    Track date on which activities are performed
    Since users can track activities on any day, not just the current one,
    an additional field is needed to record when an activity was performed.
    This field has been added to the database, and a validation has been
    added to the model, to ensure it is always set.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_14_172336) do
+ActiveRecord::Schema.define(version: 2019_08_20_122753) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_08_14_172336) do
     t.bigint "habit_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.date "performed_at", null: false
     t.index ["habit_id"], name: "index_activities_on_habit_id"
   end
 
