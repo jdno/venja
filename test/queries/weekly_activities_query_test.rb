8c3eac4d40dabde35cb55528083b7e912fc27dd2
Jan David <jandavid@6a64.com>
2019-08-21 18:26:46 +0200

    Create query object for weekly activities
    A query object has been created to wrap the necessary queries to create
    the weekly dashboard.

---
+++ test/queries/weekly_activities_query_test.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class WeeklyActivitiesQueryTest < ActiveSupport::TestCase
+  setup do
+    @user = users :user
+  end
+
+  test "without params returns habits with activities in the current week" do
+    query = WeeklyActivitiesQuery.new(Habit.where(user: @user))
+    assert_equal [habits(:journal), habits(:sleep)], query.all
+  end
+
+  test "without params includes activities in the current week" do
+    query = WeeklyActivitiesQuery.new(Habit.where(name: "Journal"))
+
+    start_date = Time.zone.now.to_date.beginning_of_week
+    end_date = start_date.end_of_week
+    weekly_activities = Activity.where(
+      habit: habits(:journal),
+      performed_at: start_date..end_date
+    )
+
+    assert_equal weekly_activities, query.all.first.activities
+  end
+end
