61db2da62c752274124ac96cd53ab756628a00d7
Jan David <jandavid@6a64.com>
2019-08-21 18:26:46 +0200

    Create query object for weekly activities
    A query object has been created to wrap the necessary queries to create
    the weekly dashboard.

---
+++ app/queries/weekly_activities_query.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+class WeeklyActivitiesQuery
+  attr_reader :relation
+
+  def initialize(relation)
+    @relation = relation
+  end
+
+  def start_date
+    @start_date ||= Time.zone.now.to_date.beginning_of_week
+  end
+
+  def end_date
+    @end_date ||= start_date.end_of_week
+  end
+
+  def all
+    relation
+      .includes(:activities)
+      .order("habits.name ASC")
+      .where(activities: { performed_at: start_date..end_date })
+  end
+end
