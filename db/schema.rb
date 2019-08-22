b124c11cb53980f18fc5800207e13dea9dd150e5
Jan David <jandavid@6a64.com>
2019-08-22 13:23:12 +0200

    Limit activities to one per day per habit
    Per habit, only a single activity can be tracked per day. This is a
    design decision, not a technical one. With the current design of the
    application, it makes sense to introduce this limit, as it avoids
    certain edge cases when tracking activities on the weekly dashboard.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_20_122753) do
+ActiveRecord::Schema.define(version: 2019_08_22_081131) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_08_20_122753) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.date "performed_at", null: false
+    t.index ["habit_id", "performed_at"], name: "index_activities_on_habit_id_and_performed_at", unique: true
     t.index ["habit_id"], name: "index_activities_on_habit_id"
   end
 
