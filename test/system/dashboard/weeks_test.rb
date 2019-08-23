f9989e18e8a5c7a1488e2acd916f5d8cceb7a4bf
Jan David <jandavid@6a64.com>
2019-08-23 15:22:14 +0200

    Create and destroy activities remotely
    For a smoother user experience, activities can now be created and
    destroyed without refreshing the page. This allows users to track their
    habits quicker, with less interruptions.

--- test/system/dashboard/weeks_test.rb
+++ test/system/dashboard/weeks_test.rb
@@ -13,4 +13,20 @@ class Dashboard::WeeksTest < ApplicationSystemTestCase
     current_week = Time.zone.now.to_date.cweek
     assert_text "Week #{current_week}"
   end
+
+  test "create activity" do
+    visit track_url(as: @user)
+
+    assert_difference ["Activity.count"], 1 do
+      page.first('a[data-action="activities#create"]').click
+    end
+  end
+
+  test "destroy activity" do
+    visit track_url(as: @user)
+
+    assert_difference ["Activity.count"], -1 do
+      page.first('a[data-action="activities#destroy"]').click
+    end
+  end
 end
