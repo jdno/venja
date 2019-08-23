0cd2a369f253363b2e1b8fe51233b9318336187d
Jan David <jandavid@6a64.com>
2019-08-23 19:13:23 +0200

    Pass date to weekly activities controller
    The weekly activities controller has been extended to allow a date to be
    passed through. When a date is present, the activities for that week are
    displayed.

--- app/controllers/dashboard/weeks_controller.rb
+++ app/controllers/dashboard/weeks_controller.rb
@@ -2,8 +2,28 @@
 
 module Dashboard
   class WeeksController < ApplicationController
+    before_action :set_date
+
     def index
-      @query = WeeklyActivitiesQuery.new(policy_scope(Habit))
+      @query = WeeklyActivitiesQuery.new(
+        policy_scope(Habit),
+        start_date: @start_date
+      )
+    end
+
+    private
+
+    def set_date
+      date = date_from_params || Time.zone.now.to_date
+      @start_date = date.beginning_of_week
+    end
+
+    def date_from_params
+      return unless params.key? :date
+
+      Time.zone.parse(params[:date])&.to_date
+    rescue TypeError, ArgumentError
+      nil
     end
   end
 end
--- test/controllers/dashboard/weeks_controller_test.rb
+++ test/controllers/dashboard/weeks_controller_test.rb
@@ -7,8 +7,20 @@ class Dashboard::WeeksControllerTest < ActionDispatch::IntegrationTest
     @user = users :user
   end
 
-  test "should show weekly dashboard" do
+  test "show current weekly dashboard without param" do
     get track_url(as: @user)
     assert_response :success
+  end
+
+  test "show current weekly dashboard with invalid param" do
+    get track_url(date: "this is not a date", as: @user)
+    assert_select "h1", "Week #{Time.zone.now.to_date.cweek}"
+  end
+
+  test "show past weekly dashboard with valid param" do
+    date = "2019-01-01"
+
+    get track_url(as: @user), params: { date: date }
+    assert_select "h1", "Week #{Time.zone.parse(date).to_date.cweek}"
   end
 end
